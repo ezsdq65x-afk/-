173f15454ffe01280c13dec65d206dba41da0778
ezsdq65x-afk <ezsdq65x@gmail.com>
2026-09-24 13:28:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="fuutai-sequential-v10";
+const CACHE_NAME="fuutai-sequential-v11-10sec";
 const APP_FILES=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
